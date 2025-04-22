a8d31765b58b0229bf5feb265adcc5f9919002ea
Laura Mª rodriguez Rodríguez <lauri161998@gmail.com>
2025-04-22 16:25:51 +0200

    Añadir mbox al clicar en la x de la app

--- main.py
+++ main.py
@@ -9,35 +9,46 @@ import styles
 import locale
 import contactos
 
+
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
-       super(Main,self).__init__()
-       var.ui = Ui_venprincipal()
-       var.ui.setupUi(self)
-       self.setStyleSheet(styles.load_stylesheet())
-       conexion.Conexion.db_conexion(self)
-       var.dlgAbrir = FileDialogAbrir()
-       var.dlgAbout = dlgAbout()
-       
-       '''
-       evento de ventana principal
-       '''
-       var.ui.actionSalir.triggered.connect(eventos.Eventos.cerrarVentana)
-       
-       '''
-       eventos del menubar y toolbar
-       '''
-       var.ui.actionSalir.triggered.connect(eventos.Eventos.mensajeSalir)
-       var.ui.actionAcercaDe.triggered.connect(eventos.Eventos.abrirAbout)
-       
-       '''
-       eventos de cajas de texto
-       '''
-       var.ui.txtemail.textChanged.connect(lambda: contactos.Contactos.checkEmail(var.ui.txtemail.text()))
-       var.ui.txtmovil.textChanged.connect(lambda: contactos.Contactos.checkMovil(var.ui.txtmovil.text()))
-    
-if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-    window = Main()
-    window.showMaximized()
-    sys.exit(app.exec())
+        super(Main, self).__init__()
+        var.ui = Ui_venprincipal()
+        var.ui.setupUi(self)
+        self.setStyleSheet(styles.load_stylesheet())
+        conexion.Conexion.db_conexion(self)
+        var.dlgAbrir = FileDialogAbrir()
+        var.dlgAbout = dlgAbout()
+
+    def closeEvent(self, event):
+        mbox = QtWidgets.QMessageBox()
+        mbox.setIcon(QtWidgets.QMessageBox.Icon.Question)
+        mbox.setWindowIcon(QtGui.QIcon('./img/logo.ico'))
+        mbox.setWindowTitle('Salir')
+        mbox.setText('¿Desea Salir?')
+        mbox.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No)
+        mbox.setDefaultButton(QtWidgets.QMessageBox.StandardButton.No)
+        mbox.button(QtWidgets.QMessageBox.StandardButton.Yes).setText('Si')
+        mbox.button(QtWidgets.QMessageBox.StandardButton.No).setText('No')
+        if mbox.exec() == QtWidgets.QMessageBox.StandardButton.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+        '''
+        eventos del menubar y toolbar
+        '''
+        var.ui.actionSalir.triggered.connect(eventos.Eventos.mensajeSalir)
+        var.ui.actionAcercaDe.triggered.connect(eventos.Eventos.abrirAbout)
+
+        '''
+        eventos de cajas de texto
+        '''
+        var.ui.txtemail.textChanged.connect(lambda: contactos.Contactos.checkEmail(var.ui.txtemail.text()))
+        var.ui.txtmovil.textChanged.connect(lambda: contactos.Contactos.checkMovil(var.ui.txtmovil.text()))
+
+    if __name__ == '__main__':
+        app = QtWidgets.QApplication([])
+        window = Main()
+        window.showMaximized()
+        sys.exit(app.exec())
